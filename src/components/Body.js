c407bc808573eaa27eb1653cc02d88ed95ef616d
Nihal Rian Dias <nihaldias261@gmail.com>
2022-10-28 02:10:35 +0530

    Added elements

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,21 @@
-import { Text, useColorMode, Button, Image, Spacer, Link, Center, Box, HStack, Heading, VStack, SimpleGrid, useColorModeValue } from "@chakra-ui/react"
-import { FaEnvelope, FaLinkedin, FaGithub, FaTwitter, FaPenSquare } from "react-icons/fa"
+import { Text, useColorMode, Button, Image, Spacer, Link, Center, Box, HStack, Heading, VStack, SimpleGrid, useColorModeValue, UnorderedList, ListItem, Collapse } from "@chakra-ui/react"
+import React from "react";
+import { FaEnvelope, FaLinkedin, FaGithub, FaTwitter, FaPenSquare, FaMicrosoft } from "react-icons/fa"
 
 function Body() {
     const { colorMode } = useColorMode('dark');
     const recOnLinkedin = useColorModeValue('blue','#34ebde')
+    const [show, setShow] = React.useState(false)
+
+    const handleToggle = () => setShow(!show)
+    
+    const [showExp, setShowExp] = React.useState(false)
+
+  const handleToggleExp = () => setShowExp(!showExp)
+    // const timeLine = useColorModeValue('blue','#34ebde')
     // const githubButton = useColorModeValue('black', 'white')
     return (
-      <Center mt={'10'}>
+      <Center my={'10'}>
         <VStack spacing={'100'}>
         <HStack>
         <Image
@@ -64,9 +73,10 @@ function Body() {
                 </VStack>
             
         </HStack>
+        <VStack spacing={10}>
         <Box>
         <Center mb={'10'}>
-            <Heading>
+            <Heading size={'2xl'}>
                 What people say
             </Heading>
         </Center>
@@ -201,30 +211,140 @@ function Body() {
         </HStack>
         </Box>
         </SimpleGrid>
+        </VStack>
         
-        <HStack>
-        <Image
+        <VStack spacing={10}>
+            <Center>
+            <Heading size={'2xl'}>Experience</Heading>
+            </Center>
+            <HStack>
+ 
+                <Image
+                alignSelf={'center'}
             borderRadius='full'
-            boxSize='xs'
+            boxSize='md'
             src='/Experience.svg'
             alt='Experience'
             />
+        
             <Spacer></Spacer>
-            <VStack align={'flex-start'}>
-                <Heading size={'2xl'}>Experience</Heading>
-                <Spacer></Spacer>
-                <Text maxWidth={['500']}>
-                I love to work on projects where I can not only leverage my Machine Learning and 
-                Software Development skills to build intelligent and accessible software solutions in 
-                the form of websites and/or mobile apps but also learn something new and exciting.
-                Aside from empowering and mentoring my fellow students while being a Microsoft Learn 
-                Student Ambassador, I enjoy writing songs as well as novels & tech articles and teaching 
-                students how to play the piano.
-                </Text>
+            <VStack align={'flex-start'} spacing={6}>
+            <Box   
+                boxShadow={colorMode === 'light' ? "1px 3px 5px 2px #dddddd" : "dark-lg"}
+                borderRadius="50px"
+                alignSelf={'center'}
+                mx={8}
+                px={10} py={5}>
+                    <VStack align={'flex-start'} spacing={3}>
+                    <Heading size = 'lg'>SDE Intern, Groupot</Heading>
+                        <Heading size={'md'} maxWidth={['400']} color={recOnLinkedin}>
+                        06/22 - Present
+                        </Heading>
+                        <Text fontSize={'md'} fontWeight={'semibold'} maxWidth={['500']}>
+                        As part of Groupot, I work on the full stack development of their software product
+                        </Text> 
 
+                        </VStack>
+                </Box>
+                <Box   
+                boxShadow={colorMode === 'light' ? "1px 3px 5px 2px #dddddd" : "dark-lg"}
+                borderRadius="50px"
+                alignSelf={'center'}
+                mx={8}
+                px={10} py={5}>
+                    <VStack align={'flex-start'} spacing={3}>
+                    <Heading size = 'lg'>Intern - IT, 60mm</Heading>
+                        <Heading size={'md'} maxWidth={['400']} color={recOnLinkedin}>
+                        09/21 - 03/22
+                        </Heading>
+                        <Text fontSize={'md'} fontWeight={'semibold'} maxWidth={['500']}>
+                        As a part of 60mm I worked on creating and testing scalable and 
+                        accessible software designed using Flutter and given functionality 
+                        through NodeJS and Firebase
+                        </Text> 
+
+                        </VStack>
+                </Box>
+                <Box   
+                boxShadow={colorMode === 'light' ? "1px 3px 5px 2px #dddddd" : "dark-lg"}
+                borderRadius="50px"
+                alignSelf={'center'}
+                mx={8}
+                px={10} py={5}>
+                    <VStack align={'flex-start'} spacing={3}>
+                    <Heading size = 'lg' maxWidth={['400']}>Flutter UI Developer, ALLSKIER Solutions Pvt Ltd</Heading>
+                        <Heading size={'md'} maxWidth={['400']} color={recOnLinkedin}>
+                        07/21 - 09/21
+                        </Heading>
+                        <Text fontSize={'md'} fontWeight={'semibold'} maxWidth={['500']}>
+                        <Collapse startingHeight={25} in={show}>
+                        As a part of ALLSKIER I have,
+                        
+                        <UnorderedList>
+                            <ListItem>
+                            Worked on building cross-platform IoT based apps 
+                            that interface with hardware to create fully-fledged 
+                            Edge Computing solutions.
+                            </ListItem>
+                            <ListItem>
+                            Conducted a webinar, "Fly with Flutter" where I explained 
+                            the basics of Flutter and App Development.
+                                </ListItem>
+                            <ListItem>
+                            Conducted a webinar, "Cloud Computing with Microsoft Azure" 
+                            as part of their "7 days 7 stories" event
+                            where I explained the basics of Cloud Computing and Creating 
+                            a VM on Microsoft Azure.
+                                </ListItem>
+                            <ListItem>
+                            Co-authored a research paper titled "Grow-IoT (smart analytics
+                            app for comprehensive plant health analysis and remote farm 
+                            monitoring using smart sensors)", which was presented at the 
+                            International Conference on Artificial Intelligence, Computational 
+                            Electronics and Communication System (AICECS 2021) and was 
+                            published in IOP: Journal of Physics, a conference proceedings 
+                            journal by the Institute of Physics, United Kingdom.
+                                </ListItem>
+                        </UnorderedList>
+                        </Collapse>
+                        <Button size='sm' onClick={handleToggle} mt='1rem'>
+                                    Show {show ? 'less' : 'more'}
+                                </Button>
+                        </Text> 
+
+                        </VStack>
+                </Box>
+                
+               
+      <Collapse in={showExp} animateOpacity>
+      <Box   
+                boxShadow={colorMode === 'light' ? "1px 3px 5px 2px #dddddd" : "dark-lg"}
+                borderRadius="50px"
+                alignSelf={'center'}
+                mx={8}
+                mt={3}
+                mb={12}
+                px={10} py={5}>
+                    <VStack align={'flex-start'} spacing={3}>
+                    <Heading size = 'lg' maxWidth={['500']}>AI Team Member, RUGVED Systems</Heading>
+                        <Heading size={'md'} maxWidth={['400']} color={recOnLinkedin}>
+                        2019 - 2020
+                        </Heading>
+                        <Text fontSize={'md'} fontWeight={'semibold'} maxWidth={['500']}>
+                        RUGVED is a Student Project recognized by MIT Manipal, that works on 
+                        Autonomous Robotics. I worked on implementing Computer Vision as well as 
+                        Mapping in autonomous robots.
+                        </Text> 
+
+                        </VStack>
+                </Box>
+      </Collapse>
+                <Button alignSelf={'center'} onClick={handleToggleExp}>Show {showExp ? 'less' : 'more'}</Button>
                 </VStack>
             
         </HStack>
+        </VStack>
+        
         <HStack>
         
             <VStack align={'flex-start'}>
@@ -252,29 +372,136 @@ function Body() {
             
         </HStack>
 
-        <HStack>
-        <Image
+        <VStack spacing={10}>
+            <Center>
+            <Heading size={'2xl'}>Projects</Heading>
+            </Center>
+            <HStack>
+ 
+                <Image
+                alignSelf={'center'}
             borderRadius='full'
-            boxSize='xs'
+            boxSize='md'
             src='/Projects.svg'
             alt='Projects'
             />
+        
             <Spacer></Spacer>
-            <VStack align={'flex-start'}>
-                <Heading size={'2xl'}>Projects</Heading>
-                <Spacer></Spacer>
-                <Text maxWidth={['500']}>
-                I love to work on projects where I can not only leverage my Machine Learning and 
-                Software Development skills to build intelligent and accessible software solutions in 
-                the form of websites and/or mobile apps but also learn something new and exciting.
-                Aside from empowering and mentoring my fellow students while being a Microsoft Learn 
-                Student Ambassador, I enjoy writing songs as well as novels & tech articles and teaching 
-                students how to play the piano.
-                </Text>
+            <VStack align={'flex-start'} spacing={6}>
+            <Box   
+                boxShadow={colorMode === 'light' ? "1px 3px 5px 2px #dddddd" : "dark-lg"}
+                borderRadius="50px"
+                alignSelf={'center'}
+                mx={8}
+                px={10} py={5}>
+                    <VStack align={'flex-start'} spacing={3}>
+                    <Heading size = 'lg' maxWidth={['400']}>Microsoft Learn module for the Azure Health Bot</Heading>
+                        <Text fontSize={'md'} fontWeight={'semibold'} maxWidth={['500']}>
+                        I co-wrote the 'Language understanding in Azure Health Bot' module for the 'Create intelligent
+                        health bots with Azure Health Bot' Learning path on the Microsoft Learn website
+                        </Text>
+                        <Text fontSize={'md'} color={recOnLinkedin} fontWeight={'semibold'} maxWidth={['500']}>
+                        <Link href='https://docs.microsoft.com/learn/modules/health-bot-language-understanding/' isExternal>
+                        <HStack>
+                            <Text fontSize={'md'} color={recOnLinkedin} fontWeight={'semibold'} maxWidth={['500']}>
+                            View the module on Microsoft Learn
+                                </Text> 
+                                <FaMicrosoft />
+                                </HStack>
+                        </Link>
+                        </Text>
+                        </VStack>
+                </Box>
+                <Box   
+                boxShadow={colorMode === 'light' ? "1px 3px 5px 2px #dddddd" : "dark-lg"}
+                borderRadius="50px"
+                alignSelf={'center'}
+                mx={8}
+                px={10} py={5}>
+                    <VStack align={'flex-start'} spacing={3}>
+                    <Heading size = 'lg' maxWidth={['400']}>Donate Life</Heading>
+                        <Text fontSize={'md'} fontWeight={'semibold'} maxWidth={['500']}>
+                        Donate Life is a cross-platform mobile app built using Flutter that helps make 
+                        blood donation an easier and more accessible experience. The flutter_tts library 
+                        for text to speech conversion is used to make the app more accessible.
+                        </Text>
+                        <Text fontSize={'md'} color={recOnLinkedin} fontWeight={'semibold'} maxWidth={['500']}>
+                        <Link href='https://github.com/nrd26/donate_life' isExternal>
+                        <HStack>
+                            <Text fontSize={'md'} color={recOnLinkedin} fontWeight={'semibold'} maxWidth={['500']}>
+                                View the project on GitHub
+                                </Text> 
+                                <FaGithub />
+                                </HStack>
+                        </Link>
+                        </Text>
 
+                        </VStack>
+                </Box>
+                <Box   
+                boxShadow={colorMode === 'light' ? "1px 3px 5px 2px #dddddd" : "dark-lg"}
+                borderRadius="50px"
+                alignSelf={'center'}
+                mx={8}
+                px={10} py={5}>
+                    <VStack align={'flex-start'} spacing={3}>
+                    <Heading size = 'lg' maxWidth={['400']}>Neko Eats</Heading>
+                        <Text fontSize={'md'} fontWeight={'semibold'} maxWidth={['500']}>
+                        A cross-platform, customizable mobile app built using Flutter that 
+                        aims to prevent customers from paying extra and restaurant owners 
+                        from earning less due to intermediary costs
+                        </Text>
+                        <Text fontSize={'md'} color={recOnLinkedin} fontWeight={'semibold'} maxWidth={['500']}>
+                        <Link href='https://github.com/nrd26/Neko-Eats' isExternal>
+                        <HStack>
+                            <Text fontSize={'md'} color={recOnLinkedin} fontWeight={'semibold'} maxWidth={['500']}>
+                                View the project on GitHub
+                                </Text> 
+                                <FaGithub />
+                                </HStack>
+                        </Link>
+                        </Text>
+
+                        </VStack>
+                </Box>
+                
+               
+      <Collapse in={showExp} animateOpacity>
+      <Box   
+                boxShadow={colorMode === 'light' ? "1px 3px 5px 2px #dddddd" : "dark-lg"}
+                borderRadius="50px"
+                alignSelf={'center'}
+                mx={8}
+                mt={3}
+                mb={12}
+                px={10} py={5}>
+                    <VStack align={'flex-start'} spacing={3}>
+                    <Heading size = 'lg' maxWidth={['400']}>Movie Recommendation System Web App</Heading>
+                        <Text fontSize={'md'} fontWeight={'semibold'} maxWidth={['500']}>
+                        I built a content-based movie recommendation system that implements a TF-IDF 
+                        Vectorizer to predict related movies based on metadata and hosted it on Microsoft 
+                        Azure as a web app using the Azure App Service. The web app is built using Flask 
+                        for the backend, Python for prediction and HTML, CSS and Jinja for the frontend.
+                        </Text>
+                        <Text fontSize={'md'} color={recOnLinkedin} fontWeight={'semibold'} maxWidth={['500']}>
+                        <Link href='https://github.com/nrd26/Recommender-Web-App' isExternal>
+                        <HStack>
+                            <Text fontSize={'md'} color={recOnLinkedin} fontWeight={'semibold'} maxWidth={['500']}>
+                                View the project on GitHub
+                                </Text> 
+                                <FaGithub />
+                                </HStack>
+                        </Link>
+                        </Text>
+
+                        </VStack>
+                </Box>
+      </Collapse>
+                <Button alignSelf={'center'} onClick={handleToggleExp}>Show {showExp ? 'less' : 'more'}</Button>
                 </VStack>
             
         </HStack>
+        </VStack>
         </VStack>
       </Center>
     );
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,14 @@
-import { Center, Heading, HStack, Image, Spacer, Text, VStack } from "@chakra-ui/react";
+import { Box, Center, Heading, HStack, Image, ListItem, Spacer, Text, UnorderedList, useColorMode, useColorModeValue, VStack } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { FaArrowRight } from "react-icons/fa"
 
 function About() {
+    const { colorMode } = useColorMode('dark');
+    const recOnLinkedin = useColorModeValue('blue','#34ebde')
+
   return (
     <Center>
-        <VStack>
+        <VStack spacing={10}>
         <HStack>
         <Image
             borderRadius='full'
@@ -29,23 +34,53 @@ function About() {
                 </VStack>
             
         </HStack>
+        <VStack spacing={10}>
+            <Center>
+            <Heading size={'2xl'}>Education</Heading>
+            </Center>
         <HStack>
         
-            <VStack align={'flex-start'}>
-                <Heading size={'2xl'}>Education</Heading>
-                <Spacer></Spacer>
-                <Text maxWidth={['500']}>
-                I love to work on projects where I can not only leverage my Machine Learning and 
-                Software Development skills to build intelligent and accessible software solutions in 
-                the form of websites and/or mobile apps but also learn something new and exciting.
-                Aside from empowering and mentoring my fellow students while being a Microsoft Learn 
-                Student Ambassador, I enjoy writing songs as well as novels & tech articles and teaching 
-                students how to play the piano.
-                I'm also skilled in French with a DELF A1 Certification awarded by CIEP.
-                </Text>
+            <VStack align={'flex-start'} spacing={10}>
+            <Box   
+                boxShadow={colorMode === 'light' ? "1px 3px 5px 2px #dddddd" : "dark-lg"}
+                borderRadius="50px"
+                alignSelf={'center'}
+                mx={8}
+                px={10} py={5}>
+                    <VStack align={'flex-start'} spacing={3}>
+                    <Heading size = 'lg' maxWidth={['500']}>B. Tech, Computer and Communication Engineering</Heading>
+                    <Heading size = 'md' maxWidth={['500']}>Manipal Institute of Technology, Manipal</Heading>
+                        <Heading size={'md'} maxWidth={['400']} color={recOnLinkedin}>
+                        07/19 - Present
+                        </Heading>
+                        <Text fontSize={'md'} maxWidth={['500']}>
+                            <UnorderedList>
+                                <ListItem><Text as={'b'}>CGPA</Text>: 8.06</ListItem>
+                                <ListItem><Text as={'b'}>Minor Specialization</Text>: Big Data</ListItem>
+                            </UnorderedList>
+                        
+                        
+                        </Text> 
+
+                        </VStack>
+                </Box>
+                <Box   
+                boxShadow={colorMode === 'light' ? "1px 3px 5px 2px #dddddd" : "dark-lg"}
+                borderRadius="50px"
+                alignSelf={'center'}
+                mx={8}
+                px={10} py={5} pr={'200'}>
+                    <VStack align={'flex-start'} spacing={3} width={['1000']}>
+                    <Heading size = 'lg' maxWidth={['500']}>Classes 1-12</Heading>
+                    <Heading size = 'md' maxWidth={['500']}>St. Aloysius Institutions, Mangalore</Heading>
+                        <Heading size={'md'} maxWidth={['400']} color={recOnLinkedin}>
+                        2007 - 2019
+                        </Heading>
+                        </VStack>
+                </Box>
 
                 </VStack>
-                <Spacer></Spacer>
+                <Spacer/>
                 <Image
             borderRadius='full'
             boxSize='xs'
@@ -53,33 +88,51 @@ function About() {
             alt='Education'
             />
             
-            
         </HStack>
+        </VStack>
+        <VStack spacing={6}>
+            <Center>
+            <Heading size={'2xl'}>Research</Heading>
+            </Center>
         <HStack>
         <Image
             borderRadius='full'
             boxSize='xs'
             src='/Research.svg'
-            alt='About me'
+            alt='Research'
             />
             <Spacer></Spacer>
             <VStack align={'flex-start'}>
-                <Heading size={'2xl'}>Research</Heading>
-                <Spacer></Spacer>
-                <Text maxWidth={['500']}>
-                I love to work on projects where I can not only leverage my Machine Learning and 
-                Software Development skills to build intelligent and accessible software solutions in 
-                the form of websites and/or mobile apps but also learn something new and exciting.
-                </Text>
-                <Text maxWidth={['500']}>
-                Aside from empowering and mentoring my fellow students while being a Microsoft Learn 
-                Student Ambassador, I enjoy writing songs as well as novels & tech articles and teaching 
-                students how to play the piano.
-                </Text>
+            <Box   
+                boxShadow={colorMode === 'light' ? "1px 3px 5px 2px #dddddd" : "dark-lg"}
+                borderRadius="50px"
+                alignSelf={'center'}
+                mx={8}
+                px={10} py={5}>
+                    <VStack align={'flex-start'} spacing={3}>
+                    <Heading size = 'lg' maxWidth={['500']}>Grow-IoT (smart analytics app 
+                    for comprehensive plant health analysis and remote farm monitoring using smart sensors)
+                    </Heading>
+                        <Text fontSize={'md'} fontWeight={'semibold'} maxWidth={['500']}>
+                        Published in 'Journal of Physics: Conference Series' under licence by IOP Publishing Ltd
+                        </Text>
+                        <Text fontSize={'md'} color={recOnLinkedin} fontWeight={'semibold'} maxWidth={['500']}>
+                        <Link href='https://iopscience.iop.org/article/10.1088/1742-6596/2161/1/012059' isExternal>
+                        <HStack>
+                            <Text fontSize={'md'} color={recOnLinkedin} fontWeight={'semibold'} maxWidth={['500']}>
+                            View the Research Paper
+                                </Text> 
+                            <FaArrowRight/>
+                                </HStack>
+                        </Link>
+                        </Text>
+                        </VStack>
+                </Box>
 
                 </VStack>
             
         </HStack>
+        </VStack>
         <HStack>
         
             
